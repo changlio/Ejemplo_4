af9886785082a7a599ee7fe1aca996ef44c4a143
changlio <4166395@studenti.unige.it>
2020-05-03 18:43:17 +0200

    el java file

---
+++ Ejemplo_1.java
@@ -0,0 +1,29 @@
+//este fue el ejercicio # 1, para practicar funciones
+class Ejemplo_1 {
+
+public static void ejemplo(){
+System.out.println("Chin");
+}
+
+static int ejemplo2(int numero1,int numero2){
+return numero1+numero2;
+}
+
+
+
+    public static void main(String args[]){
+int valor = 15;
+String nombre;
+String apellido;
+String nombre_completo;
+nombre= "joselito ";
+apellido="flores";
+
+        System.out.println(nombre + apellido);
+	System.out.println("Que ondas");
+	System.out.println(ejemplo2(2,3));
+	ejemplo();
+    }
+}
+
+
